b8882467826449f2a01b070ddbe8e16dd3538bd4
psheehan89 <psheehan89@gmail.com>
2017-01-03 12:10:39 -0800

    Progress: Create rooms and solve XSS attack issues

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,12 +2,13 @@
 class App {
   constructor() {
     this.server = 'https://api.parse.com/1/classes/messages';
+    this.friends = [];
   }
   
   init () {
     console.log('We have init!');
     $('.username').off('click').on('click', function() {
-      this.handleUsernameClick();
+      this.handleUsernameClick($('.username').val());
     }.bind(this));
 
     $('#send').off('submit').on('submit', function(event) {
@@ -21,6 +22,17 @@ class App {
     }.bind(this));
 
     $('#refresh').off('click').on('click', function() {
+      this.clearMessages();
+      this.fetch();
+    }.bind(this));    
+
+    $('#roomSelect').on('change', function() {
+      if ($('#roomSelect').val() === 'createNewRoom') {
+        console.log('new room');
+        var newRoom = prompt('What is the name of your new room?');
+        this.renderRoom(newRoom);
+        $('#roomSelect').val(newRoom).change();
+      }
       this.clearMessages();
       this.fetch();
     }.bind(this));
@@ -60,8 +72,12 @@ class App {
       success: function (data) {
         console.log('chatterbox: Messages fetched', data);
         var count = 0;
-        for (var i = 0; i < 30; i++) {
-          if (data.results[i].roomname === $('#roomSelect').val() && count < 30) {
+        for (var i = 0; i < 100; i++) {
+          if (!$(`#roomSelect option[value="${this.escapeHTML(data.results[i].roomname)}"]`).length) {
+            this.renderRoom(this.escapeHTML(data.results[i].roomname));
+          }
+
+          if ((data.results[i].roomname) === $('#roomSelect').val() && count < 100) {
             this.renderMessage(data.results[i]);
             count++;
           }
@@ -78,18 +94,36 @@ class App {
     $('#chats').html('');
   }
 
+  escapeHTML(value) {
+    var div = document.createElement('div');
+    if (value) {
+      value = value.replace(/(['"])/g, '\\$1');
+    }
+    div.appendChild(document.createTextNode(value));
+    return div.innerHTML;
+  }
+
+
   renderMessage(message) {
+
+    
+
     //var post = `<div class="message"><p class="username">${message.username}</p><p>${message.text}</p><p>${message.roomname}</p></div>`;
-    var post = `<div class="message"><p class="username">${message.username}</p><p>${message.text}</p><p>${message.roomname}</p></div>`;
+    var post = `<div class="message"><p class="username">${this.escapeHTML(message.username)}</p><p>${this.escapeHTML(message.text)}</p></div>`;
     $('#chats').append(post);
   }
+
   renderRoom(name) {
     $('#roomSelect').append(`<option value="${name}">${name}</option>`);
-
   }
 
-  handleUsernameClick() {
+  handleUsernameClick(username) {
     console.log('username clicked');
+
+    this.friends.push(username);
+
+    console.log(this.friends);
+
   }
 
   handleSubmit(input) {
@@ -99,6 +133,6 @@ class App {
   }
 }
 
-//var app = new App();
+var app = new App();
 
 //app.init();
